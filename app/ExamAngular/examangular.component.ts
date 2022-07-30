f00e3f6dda7a58cbd87458a1c789be444ad1773b
Nguyen Linh <105443340+Vndark5464@users.noreply.github.com>
2022-07-30 10:30:23 +0700

    Add files via upload

---
+++ app/ExamAngular/examangular.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-examangular',
+    templateUrl: 'examangular.component.html'
+})
+
+export class ExamangularComponent{
+
+}
